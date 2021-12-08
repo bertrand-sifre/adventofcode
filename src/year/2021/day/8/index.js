8a8c3580a07e85c385e271878baeafdb93203090
Bertrand Sifre <bertrand.sifre@gmail.com>
2021-12-08 13:34:24 +0100

    2021 day 8

--- src/year/2021/day/8/index.js
+++ src/year/2021/day/8/index.js
@@ -1,4 +1,142 @@
+const numberToSegment = [
+  'abcefg', // 0
+  'cf',//1
+  'acdeg',//2
+  'acdfg',// 3
+  'bcdf',// 4
+  'abdfg',// 5
+  'abdefg',// 6
+  'acf',// 7
+  'abcdefg',// 8
+  'abcdfg',// 9
+]
+
+const extract = (values) => {
+  return values.map(v => {
+    const s = v.split(' | ')
+    return {
+      pattern: s[0].split(' '),
+      out: s[1].split(' '),
+    }
+  })
+}
+
+/**
+ * @param {string[]} str
+ * @param {string[]} exclude
+ * @param {number} nb
+ */
+const compareStr = function (str, nb = 1, exclude = []) {
+  const charList = (str.join('')).split('').filter((item, pos, self) => self.indexOf(item) === pos).join('')
+  let diff = ""
+  for (let i = 0; i < charList.length; i++) {
+    let j = 0
+    str.forEach(s => {
+      if (!exclude.includes(charList[i]) && s.includes(charList[i])) {
+        j++
+      }
+    })
+    if (j === nb) {
+      diff += charList[i]
+    }
+  }
+  return diff
+}
+
+const findA = (input) => {
+  const _2 = input.pattern.filter(a => a.length === 2)[0]
+  const _3 = input.pattern.filter(a => a.length === 3)[0]
+  return compareStr([_2, _3])
+}
+
+const findE = (input) => {
+  const _5 = input.pattern.filter(a => a.length === 5)
+  const _4 = input.pattern.filter(a => a.length === 4)
+  return compareStr(_5.concat(_4))
+}
+
+const findD = (input) => {
+  const _5 = input.pattern.filter(a => a.length === 5)
+  const _4 = input.pattern.filter(a => a.length === 4)
+  return compareStr(_5.concat(_4), 4)
+}
+
+const findB = (input, d) => {
+  const _4 = input.pattern.filter(a => a.length === 4)
+  const _2 = input.pattern.filter(a => a.length === 2)
+  return compareStr(_2.concat(_4), 1, [d])
+}
+
+const findC = (input, f) => {
+  const _2 = input.pattern.filter(a => a.length === 2)
+  return compareStr(_2, 1, [f])
+}
+const findF = (input, array) => {
+  const _6 = input.pattern.filter(a => a.length === 6)
+  return compareStr(_6, 3, array)
+}
+const findG = (input, a) => {
+  const _6 = input.pattern.filter(a => a.length === 6)
+  const _5 = input.pattern.filter(a => a.length === 5)
+  return compareStr(_6.concat(_5), 6, [a])
+
+}
+
+const convertSegment = (str, exchange) => {
+  return str.split('').map(c => exchange[c]).sort().join('')
+}
+
 module.exports.computeLevel1 = (values) => {
+  const inputs = extract(values)
+  let rst = 0
+  inputs.forEach(input => {
+    const a = findA(input)
+    const e = findE(input)
+    const d = findD(input)
+    const b = findB(input, d)
+    const g = findG(input, a)
+    const f = findF(input, [a, b, g])
+    const c = findC(input, f)
+    const exchange = {}
+    exchange[a] = 'a'
+    exchange[b] = 'b'
+    exchange[c] = 'c'
+    exchange[d] = 'd'
+    exchange[e] = 'e'
+    exchange[f] = 'f'
+    exchange[g] = 'g'
+    const segments = input.out.map(o => convertSegment(o, exchange))
+    segments.forEach(segment => {
+      const value = numberToSegment.indexOf(segment)
+      if (value === 1 || value === 4 || value === 7 || value == 8) {
+        rst++
+      }
+    })
+  })
+  return rst
 }
 module.exports.computeLevel2 = (values) => {
+  const inputs = extract(values)
+  let rst = 0
+  inputs.forEach(input => {
+    const a = findA(input)
+    const e = findE(input)
+    const d = findD(input)
+    const b = findB(input, d)
+    const g = findG(input, a)
+    const f = findF(input, [a, b, g])
+    const c = findC(input, f)
+    const exchange = {}
+    exchange[a] = 'a'
+    exchange[b] = 'b'
+    exchange[c] = 'c'
+    exchange[d] = 'd'
+    exchange[e] = 'e'
+    exchange[f] = 'f'
+    exchange[g] = 'g'
+    const segments = input.out.map(o => convertSegment(o, exchange))
+    const nb = +segments.map(segment => numberToSegment.indexOf(segment)).join('')
+    rst += nb
+  })
+  return rst
 }
---
+++ src/year/2021/day/8/index.spec.js
@@ -0,0 +1,23 @@
+const sample = ["be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+  "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+  "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+  "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+  "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+  "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+  "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+  "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+  "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+  "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce",]
+
+const code = require('./index')
+
+describe('2021 day 8', () => {
+  it('valid sample level 1', () => {
+    const rst = code.computeLevel1(sample)
+    expect(rst).toEqual(26)
+  })
+  it('valid sample level 2', () => {
+    const rst = code.computeLevel2(sample)
+    expect(rst).toEqual(61229)
+  })
+})
